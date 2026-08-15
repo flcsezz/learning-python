3fd08d7067938d73cfda606e1002ad29efb19f28
flcsezz <mavrick7193@gmail.com>
2026-08-15 14:09:10 +0530

    alien conditionsl statements

---
+++ alien.py
@@ -0,0 +1,7 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print('earnd 5 pointsfor shooting the alien')
+elif alien_color=="yellow":
+    print("player just eaned 10 points")
+elif alien_color=='red':
+    print('player earned 15 points')
