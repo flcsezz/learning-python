7604c8972231f10523685a468537ebaeb28df1a9
flcsezz <mavrick7193@gmail.com>
2026-08-15 14:24:47 +0530

    conditional+loops

---
+++ lifesrt.py
@@ -0,0 +1,11 @@
+age = 67
+if age<2:
+    print('aww ur just a babyyy')
+elif age>2 and age <4:
+    print("you are a toddler")
+elif age>=13 and age < 20:
+    print("Person is a teenager")
+elif age >=20 and age < 65:
+    print("person is a adult")
+if age>=65:
+    print("why are you still alive? just go alredy gng aint nothin left here for you")
