f0b9778216aef5c47e5e3d1b4380f8ca3e11343d
flcsezz <mavrick7193@gmail.com>
2026-08-16 12:14:44 +0530

    201 pages done

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,11 @@
-person = {
-    'age': 30,
-    'city': 'solan',
-    'first_name' : 'pav',
-    'last_name' : 'bhaji',
-
+grail = {
+    'srings' : "strings are series of characters enclose in double quotes",
+    'methods' : 'methods are functions defined for a specific object or data type',
+    'function' : 'function are reusable block of code we use them because of DRY rule',
+    "DRY"      : 'Dont Repeat Yourself rule',
+    'PEP8'     : 'PEP8 is a pyhton styling guide that python deves use to make better and readable code'
+    
 }
-for i in person.values():
-    print(i)
 
-
-
-'''
-print(person['age'])
-print(person['city'])
-print(person['first_name'])
-print(person['last_name'])
-'''
+for k, v in grail.items():
+    print(f'\t{k}\n\t>{v}\n')
