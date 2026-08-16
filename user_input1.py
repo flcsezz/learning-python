ee73a706de40a523c6f95419c152f7b168233645
flcsezz <mavrick7193@gmail.com>
2026-08-16 17:43:57 +0530

    starting input() next is while loops

---
+++ user_input1.py
@@ -0,0 +1,7 @@
+car  = input("How many people are in your dinner group : ")
+car = int(car)
+
+if car > 8:
+    print("sorry were fulll rn yall have to wait: ")
+else:
+    print("your table is ready")
