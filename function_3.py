a61c1f30fcfe3885878084ac5b9e7c29a81fa711
flcsezz <mavrick7193@gmail.com>
2026-08-21 19:18:30 +0530

    finishing functions

---
+++ function_3.py
@@ -0,0 +1,7 @@
+def sandwitch(*items):
+    print("\nThis Sandwitch will be made of")
+    for it in items:
+        print(f"-{it}")
+
+sandwitch("nogo",'yogo' ,' geni')
+sandwitch('nlgo','notnigo')
