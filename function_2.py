483c5afb9420d914b9bb6fdc4b7d95262bd99756
flcsezz <mavrick7193@gmail.com>
2026-08-21 16:03:30 +0530

    almost done w function

--- function_2.py
+++ function_2.py
@@ -1,7 +1,26 @@
-def city_country(city, country):
-    '''this function returns a neatly formatted string'''
-    print(f'"{city.title()}, {country.title()}"')
+def make_album(artist,title,songs = None):
+    if songs:
+        album = {"artist": artist, "title": title, "Songs" : songs,}
+    else:
+        album = {"artist": artist, "title": title,}
+    return album
 
-city_country("santiago", "chile")
-city_country("jabalpur", 'india')
-city_country('Los angeles', 'USA')
+
+    
+while True:
+    print('\npress enter q anytime to exit')
+    artist = input("\nEnter artist name:")
+    if artist.lower() == "q":
+        break
+    title = input("\nEnter title name :")
+    if title.lower() == "q":
+        break
+    Songs = input ("\nHow many songs(press enter if none) : ")
+    if Songs.lower() == "q":
+        break
+    if Songs:
+        album = make_album(artist,title, Songs)
+        print(album)
+    else:
+        album = make_album(artist,title)
+        print(album)
