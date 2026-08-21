afc646cb464d52521eef72a1efecd100bc816d73
flcsezz <mavrick7193@gmail.com>
2026-08-21 15:40:58 +0530

    almost done w function

---
+++ function_2.py
@@ -0,0 +1,7 @@
+def city_country(city, country):
+    '''this function returns a neatly formatted string'''
+    print(f'"{city.title()}, {country.title()}"')
+
+city_country("santiago", "chile")
+city_country("jabalpur", 'india')
+city_country('Los angeles', 'USA')
